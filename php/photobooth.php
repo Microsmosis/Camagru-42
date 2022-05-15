f51580f0c06d8c9f3410fc7f9bc8de48b77b37c4
Luke <pilot@Luke.local>
2022-05-15 19:30:45 +0300

    addition for photobooth.php

--- php/photobooth.php
+++ php/photobooth.php
@@ -2,7 +2,7 @@
 	session_start();
 ?>
 
-<html>
+<!-- <html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -82,4 +82,43 @@
 			$(function () { start(); });
 		</script>
 	</body>
+</html> -->
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Camagru</title>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<button id="start-camera">Start Camera</button>
+		<video id="video" width="320" height="240" autoplay></video>
+		<button id="click-photo">Click Photo</button>
+		<canvas id="canvas" width="320" height="240"></canvas>
+	</body>
 </html>
+<script> 
+	let camera_button = document.querySelector("#start-camera");
+	let video = document.querySelector("#video");
+	let click_button = document.querySelector("#click-photo");
+	let canvas = document.querySelector("#canvas");
+
+	camera_button.addEventListener('click', async function() {
+		let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+		video.srcObject = stream;
+	});
+
+	click_button.addEventListener('click', function() {
+		canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+		let image_data_url = canvas.toDataURL('image/jpeg');
+
+		// data url of the image
+		console.log(image_data_url);
+	});
+</script>
