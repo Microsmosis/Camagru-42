c53259df06eb77600fe02cafeccb11592c3c43b1
Luke <pilot@Luke.local>
2022-06-04 14:28:57 +0300

    small fix for profile page image

--- php/profile_page.php
+++ php/profile_page.php
@@ -181,8 +181,9 @@
 			<?php
 				try
 				{
+					$user_profile = $_SESSION['logged_in_user'];
 					$conn = connect();
-					$sql = "SELECT img_path, img_name, img_user FROM user_images ORDER BY id DESC";
+					$sql = "SELECT img_path, img_name, img_user FROM user_images WHERE img_user='$user_profile' ORDER BY id DESC";
 					$stmt = $conn->query($sql);
 					$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 					if($result)
