fb2c4609cf2f15f34fd75bb66c036b1829f1d0d1
Luke <pilot@Luke.local>
2022-07-05 15:00:43 +0300

    send mail function path fixed

--- php/send_mail.php
+++ php/send_mail.php
@@ -5,7 +5,7 @@
 		{
 			$to = $email_adrs;
 			$subject = 'E-mail Verification';
-			$message = 'Hello new user! Good to have you with us :) Start your journey in Camagru by verifying your e-mail address by pressing the link below!' . PHP_EOL . "http://localhost:8081/guru7/php/verification.php?code=$acti_code";
+			$message = 'Hello new user! Good to have you with us :) Start your journey in Camagru by verifying your e-mail address by pressing the link below!' . PHP_EOL . "http://localhost:8081/camagru/php/verification.php?code=$acti_code";
 			mail($to, $subject, $message);
 			
 		}
@@ -13,7 +13,7 @@
 		{
 			$to = $email_adrs;
 			$subject = 'Password Reset';
-			$message = 'To reset your password, please click the link below.' . PHP_EOL . "http://localhost:8081/guru7/php/reset_form.php?key=$username&reset=$password&mail=$email_adrs";
+			$message = 'To reset your password, please click the link below.' . PHP_EOL . "http://localhost:8081/camagru/php/reset_form.php?key=$username&reset=$password&mail=$email_adrs";
 			mail($to, $subject, $message);
 			
 		}
@@ -21,7 +21,7 @@
 		{
 			$to = $email_adrs;
 			$subject = 'Someone Commented Your Image!';
-			$message = 'Someone has something to say about your post, Go and check it out!' . PHP_EOL . "http://localhost:8081/guru7";
+			$message = 'Someone has something to say about your post, Go and check it out!' . PHP_EOL . "http://localhost:8081/camagru";
 			mail($to, $subject, $message);
 			
 		}
